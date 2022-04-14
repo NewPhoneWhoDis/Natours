2f03254a6178dc7e14c2b95d4db12407bb942db0
Christian <88944722+NewPhoneWhoDis@users.noreply.github.com>
2022-04-14 22:23:00 +0200

    basic api route

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs =require('fs');
 
 const app = express();
 
@@ -6,7 +7,16 @@ app.get('/', (req, res) => {
     res.json({ message: 'Hello form the backend', app: 'Natours'});
 })
 
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
+app.get('/api/v1/tours', (req, res) => {
+    res.json({
+        results: tours.length,
+        data: {
+            tours: tours
+        }
+    });
+})
 
 
 const port = 3000;
