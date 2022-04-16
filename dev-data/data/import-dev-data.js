20373a3a66bfa242b322d5c225a0a9a43b2e5166
Christian <88944722+NewPhoneWhoDis@users.noreply.github.com>
2022-04-16 16:57:22 +0200

    added db connection with and tours schema

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,48 @@
+const dotenv = require('dotenv');
+const fs = require('fs');
+const mongoose = require('mongoose');
+const Tour = require('./../../models/toursSchema');
+
+dotenv.config({ path: './config.env' });
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => { console.log('Database connected') })
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+const importData = async () => {
+    try {
+        await Tour.create(tours);
+        console.log('Data has been successfully imported!');
+    } catch (err) {
+        console.log(err);
+    }
+
+    process.exit();
+}
+
+const deleteData = async () => {
+    try {
+        await Tour.deleteMany()
+        console.log('Data successfully deleted!');
+    } catch (err) {
+        console.log(err);
+    }
+
+    process.exit();
+}
+
+if (process.argv[2] === '--import') {
+    importData();
+} else if (process.argv[2] === '--delete') {
+    deleteData();
+}
+
+console.log(process.argv);
+
+
+
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
+const mongoose = require('mongoose');
+
 const app = require('./app');
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => { console.log('Database connected') })
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
