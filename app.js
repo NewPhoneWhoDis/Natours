65fdfe911db44c994d77a6595650ab608ce59edb
Christian <88944722+NewPhoneWhoDis@users.noreply.github.com>
2022-04-19 18:25:09 +0200

    Security Best Practices

--- app.js
+++ app.js
@@ -7,14 +7,51 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const errorHandler = require('./controllers/errorController');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = requrie('hpp');
 
 const app = express();
+
+app.use(helmet({ referrerPolicy: { policy: "no-referrer" }, }));
 
 if (process.env.NODE_ENV !== 'production') {
     app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Allows 100 requests from the same ip in 1 hour
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, chill out!'
+})
+
+app.use('/api', limiter);
+
+// Body parser that reads data from the body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization agains NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization agains XSS
+app.use(xss());
+
+// Preventing Parameter Pollution /always use at the end because it clears the query string
+app.use(hpp({
+    whitelist: [
+        'duration',
+        'ratingsQuantity',
+        'ratingsAverage',
+        'maxGroupSize',
+        'difficulty',
+        'price'
+    ]
+}));
+
+// Serve static files
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
